2637358467ba4c3a4c7dd71e429047776c444662
Jonathan Dalrymple <jonathan@Blackbook.local>
2009-04-23 19:37:18 +0100

    Commented Pare Class

--- mod/resource/type/pare/resource.class.php
+++ mod/resource/type/pare/resource.class.php
@@ -8,6 +8,8 @@ class resource_pare extends resource_base
 	function resource_pare($cmid=0)
 	{
 		parent::resource_base($cmid);
+		
+		fb('Constructor called');
 	}
 
 	function display()
@@ -15,32 +17,49 @@ class resource_pare extends resource_base
 		///Display the resource
 
 		global $CFG;
-		parent::display();
+		fb('display was called');
 		
+		parent::display();
 		
 	}
 
 	function add_instance($resource)
 	{
+		fb('add_instance called');
 		return parent::add_instance($resource);
 	}
 
 	function update_instance($resource)
 	{
+		fb('update_instance called');
 		return parent::update_instance($resource);
 	}
 
 	function delete_instance($resource)
 	{
+		fb('delete_instance called');
 		return parent::delete_instance($resource);
 	}
-
+	
+	/**
+	*	setup_elements 
+	* 	Sets up the 
+	* 
+	* 	@param $mform 
+	*/
 	function setup_elements(&$mform)
 	{
+		fb('setup_elements called');
+	    
+		$mform->addElement('htmleditor', 'alltext', get_string('fulltext', 'resource'), array('cols'=>85, 'rows'=>30));
+		
+		fb($mform);
 	}
 
 	function setup_preprocessing(&$default_values)
 	{
+		fb('setup_preprocessing called');
+		fb($default_values);
 	}
  
 }
