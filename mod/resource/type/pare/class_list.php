66b8010738e8b09f19f374e40920abc79be848f7
Jonathan Dalrymple <jonathan@Blackbook.local>
2009-04-28 19:49:03 +0100

    Added skeleton classes

---
+++ mod/resource/type/pare/class_list.php
@@ -0,0 +1,8 @@
+<?php
+
+	require_once('model.class.php');
+	require_once('resourceModel.class.php');
+	require_once('userModel.class.php');
+	require_once('courseModel.class.php');
+	
+?>
---
+++ mod/resource/type/pare/courseModel.class.php
@@ -0,0 +1,7 @@
+<?php
+
+class CourseModel extends model{
+	
+}
+
+?>
